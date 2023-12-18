0ff5df5b365eea477ee6aa0b8bc50ed65136093c
ayan <ayan.d3v@gmail.com>
2023-12-19 04:30:19 +0600

    not pano, not zoom

--- src/graphLayout.js
+++ src/graphLayout.js
@@ -1,51 +1,15 @@
 export default class ForceDirectedGraph {
-
-    MIN_X = -1000;
-    MIN_Y = -1000;
-    MAX_X = 1000;
-    MAX_Y = 1000;
-
-    MIN_SCALE = 0.5;
-    MAX_SCALE = 10;
-    ZOOM_FACTOR = 1.1;
-
     constructor(graph, canvasId) {
-        // this.WIDTH = window.innerWidth
-        // this.HEIGHT = window.innerHeight
-
         this.graph = graph;
-
         this.canvas = document.getElementById(canvasId);
         this.ctx = this.canvas.getContext('2d');
-        // this.canvas.width = this.WIDTH
-        // this.canvas.height = this.HEIGHT
-
         this.nodePositions = new Map();
-
         this.animationFrameRequest = null;
         this.repulsionForce = 80; // Сила отталкивания
         this.attractionForce = 0.1; // Сила притяжения
         this.lastMoveTimestamp = 0; // Для throttling обработки событий мыши
         this.moveThrottleInterval = 20; // Интервал в миллисекундах
-
-
-        this.windowCenter = {
-            x: this.canvas.width / 2,
-            y: this.canvas.height / 2
-        }
-        // Изначальные параметры зумирования
-        this.scale = 1;
-        this.offsetX = 0;
-        this.offsetY = 0;
-        // this.lastZoomAction = null;
-        this.zoomDelta = 1; // в чем отличие от zoom_factor? проверить потом вдруг лишняя перменная
-        this.startX = 0;
-        this.startY = 0;
-
-        this.isDragging = false;
         this.draggingNode = null;
-
-        this.#getURLParams()
         this.initializeMouseEvents();
     }
 
@@ -97,13 +61,8 @@ export default class ForceDirectedGraph {
 
 
     draw() {
-        this.ctx.save();
         // Очистите холст
         this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
-
-        // this.ctx.setTransform(this.scale, 0, 0, this.scale, this.windowCenter.x, this.windowCenter.y);
-        // this.ctx.translate(this.offsetX, this.offsetY)
-
 
         // Рисуйте ребра
         this.ctx.strokeStyle = 'black';
@@ -125,12 +84,6 @@ export default class ForceDirectedGraph {
             this.ctx.arc(position.x, position.y, 10, 0, 2 * Math.PI);
             this.ctx.fill();
         }
-
-
-        // // Обновление URL с параметрами GET
-        this.#updateURLParams();
-        this.zoomDelta = 1;
-        this.ctx.restore();
     }
 
     // Здесь должен быть код для обновления позиций узлов на основе силовой раскладки
@@ -172,7 +125,6 @@ export default class ForceDirectedGraph {
 
 
     initializeMouseEvents() {
-        this.canvas.addEventListener('wheel', (event) => this.handleZoom(event));
         this.canvas.addEventListener('mousedown', (event) => this.handleMouseDown(event));
         this.canvas.addEventListener('mousemove', (event) => this.handleMouseMove(event));
         this.canvas.addEventListener('mouseup', (event) => this.handleMouseUp(event));
@@ -189,9 +141,6 @@ export default class ForceDirectedGraph {
             }
         }
 
-        this.isDragging = true;
-        this.startX = event.clientX;
-        this.startY = event.clientY;
     }
 
     handleMouseMove(event) {
@@ -206,71 +155,11 @@ export default class ForceDirectedGraph {
             const position = this.nodePositions.get(this.draggingNode);
             position.x = event.offsetX;
             position.y = event.offsetY;
-        } else if (this.isDragging) {
-            // Calculate the distance moved by the mouse
-            // const dx = mousePosX - this.offsetX;
-            // const dy = mousePosY - this.offsetY;
-            const dx = (event.clientX - this.startX)/this.scale;
-            const dy = (event.clientY - this.startY)/this.scale;
-
-            this.offsetX += dx;
-            this.offsetY += dy;
-
-            if (this.offsetX < this.MIN_X) this.offsetX = this.MIN_X;
-            if (this.offsetX > this.MAX_X) this.offsetX = this.MAX_X;
-            if (this.offsetY < this.MIN_Y) this.offsetY = this.MIN_Y;
-            if (this.offsetY > this.MAX_Y) this.offsetY = this.MAX_Y;
-
-            this.startX = event.clientX;
-            this.startY = event.clientY;
-
-            this.draw();
-
         }
     }
 
     handleMouseUp() {
         this.draggingNode = null;
-        this.isDragging = false
-    }
-
-
-    zoom(coefficient) {
-        if (coefficient > 0) {
-            this.zoomDelta = this.ZOOM_FACTOR;
-            // this.lastZoomAction = 'zoomed in';
-        } else {
-            this.zoomDelta = 1 / this.ZOOM_FACTOR;
-            // this.lastZoomAction = 'zoomed out';
-        }
-
-        this.scale *= this.zoomDelta;
-        if (this.scale < this.MIN_SCALE) this.scale = this.MIN_SCALE;
-        if (this.scale > this.MAX_SCALE) this.scale = this.MAX_SCALE;
-
-        this.draw()
-    }
-
-    handleZoom(event) {
-        const wheelDelta = Math.max(-1, Math.min(1, (event.wheelDelta || -event.detail)));
-        this.zoom(wheelDelta)
-    }
-
-    // Обновление URL с параметрами GET
-    #updateURLParams() {
-        const params = new URLSearchParams(window.location.search);
-        params.set('scale', this.scale.toFixed(1));
-        params.set('offsetX', parseInt(this.offsetX));
-        params.set('offsetY', parseInt(this.offsetY));
-        window.history.replaceState({}, '', `${window.location.pathname}?${params}`);
-    }
-
-    // Получение параметров из URL
-    #getURLParams() {
-        const params = new URLSearchParams(window.location.search);
-        this.scale = parseFloat(params.get('scale')) || this.scale;
-        this.offsetX = parseInt(params.get('offsetX')) || this.offsetX;
-        this.offsetY = parseInt(params.get('offsetY')) || this.offsetY;
     }
 
     stop() {
